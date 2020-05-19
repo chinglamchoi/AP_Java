40dec2cf18efd0715cf4a86180d32beaeaa98f77
Ching Lam Choi <ccl5a09@gmail.com>
2020-05-19 09:46:36 +0800

    Add files via upload

---
+++ switches.java
@@ -0,0 +1,15 @@
+public class switches {
+	public static void main(String[] args) {
+		String a = "sad";
+		switch(a) {
+			case "happy": 
+				System.out.println("won't be soon");
+				break;
+			case "sad": 
+				System.out.println("don't be");
+				break;
+			default: 
+				System.out.println("damn emotions");
+		}
+	}
+}
