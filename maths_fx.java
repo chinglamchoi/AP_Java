851a3135d4f904f77e429b93c5825acf3cdaf975
Ching Lam Choi <ccl5a09@gmail.com>
2020-05-19 09:45:01 +0800

    Add files via upload

---
+++ maths_fx.java
@@ -0,0 +1,14 @@
+public class maths_fx {
+	public static void main(String[] args) {
+		System.out.println(Math.max(5, 10));
+	}
+}
+
+/*
+ Math functions:
+ - Math.max(a,b)
+ - Math.min(a,b)
+ - Math.sqrt(a)
+ - Math.abs(a)
+ - Math.random() // returns random number between [0, 1]
+ */
